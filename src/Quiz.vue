c54e0150fafdd32eed47691e995fa713e3a95b09
Fares Gdoura <gdourafares@gmail.com>
2024-07-22 23:29:06 +0100

    quizz component

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -16,32 +16,45 @@ const questions = [
 ];
 
 const currentQuestionIndex = ref(0)
-const currentQuestion = ref(questions[currentQuestionIndex.value])
+const currentQuestion = computed(() => {
+  if (!quizzDone.value)
+    return questions[currentQuestionIndex.value]
+
+  // just to prevent errors
+  else return questions[questions.length - 1]
+})
 const correctAnswers = ref(0)
 const selectedOption = ref()
-const quizzDone = computed(() => currentQuestionIndex.value === questions.length - 1)
+const quizzDone = computed(() => currentQuestionIndex.value === questions.length)
+const currentAnswerIsCorrect = computed(() => currentQuestion.value.answer === selectedOption.value)
+const currentQuestionDone = ref(false)
 const submit = () => {
   if (!selectedOption.value) return
-  if (selectedOption.value === currentQuestion.value.answer)
+
+  if (currentAnswerIsCorrect.value)
     correctAnswers.value++
 
-  // go next question or show result
-  if (quizzDone.value) showResult()
-  else currentQuestionIndex.value++
+  currentQuestionDone.value = true
 }
-const showResult = () => {
 
+const next = () => {
+  // reset stuff
+  currentQuestionIndex.value++
+  selectedOption.value = ''
+  currentQuestionDone.value = false
 }
 </script>
 <template>
-  <div class="min-w-[50vw] border rounded-2xl p-10 flex flex-col gap-10">
+  <p
+      v-show="quizzDone"
+      class="text-3xl text-center"
+  >Your final score: {{ correctAnswers }} out of {{ questions.length }}</p>
+  <div
+      v-show="!quizzDone"
+      class="min-w-[50vw] border rounded-2xl p-10 flex flex-col gap-10"
+  >
     <p class="text-2xl border-b-2 border-b-white py-2">{{ currentQuestion.question }}</p>
-    <p
-        v-show="quizzDone"
-        class="text-xl text-yellow-700 text-center"
-    >Result: {{ correctAnswers }} of {{ questions.length }} are correct!</p>
     <ul
-        v-show="!quizzDone"
         class="flex flex-col gap-4 text-xl"
     >
       <li
@@ -58,11 +71,21 @@ const showResult = () => {
       </li>
     </ul>
     <div class="flex gap-5 py-4">
-      <p class="p-4 text-center basis-4/5 bg-green-500 border-green-500 border rounded-2xl">Your answer is ...</p>
+      <p
+          v-show="currentQuestionDone"
+          :class="{'bg-green-500 border-green-500': currentAnswerIsCorrect, 'bg-red-500 border-red-500': !currentAnswerIsCorrect}"
+          class="p-4 text-center basis-4/5 border rounded-2xl "
+      >Your answer is {{ currentAnswerIsCorrect ? 'Correct' : 'Wrong' }}</p>
       <button
+          v-show="!quizzDone && !currentQuestionDone"
           class="basis-1/5 p-4 border rounded-2xl bg-blue-500 border-blue-500"
           @click="submit"
       >Submit</button>
+      <button
+          v-show="!quizzDone && currentQuestionDone"
+          class="basis-1/5 p-4 border rounded-2xl bg-blue-500 border-blue-500"
+          @click="next"
+      >Next</button>
     </div>
   </div>
 </template>
